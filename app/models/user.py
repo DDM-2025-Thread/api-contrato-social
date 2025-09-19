92fa3c037bb3614ddb9456928f258953914fcb98
gustavo-berti <gutigimenez15@gmail.com>
2025-09-18 21:25:59 -0300

    feat: adiciona modelo do usuário

---
+++ app/models/user.py
@@ -0,0 +1,12 @@
+from sqlalchemy import Column, Integer, String, DateTime
+from datetime import datetime
+from app.database import Base
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String(150))
+    email = Column(String(150), unique=True, index=True)
+    password_hash = Column(String(255))
+    created_at = Column(DateTime, default=datetime.utcnow)
