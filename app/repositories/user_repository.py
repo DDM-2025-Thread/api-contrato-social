69ad81eead6a3dc24bf316a610bfc0ae04e5ce38
gustavo-berti <gutigimenez15@gmail.com>
2025-09-25 20:19:25 -0300

    feat: cria repositorio do usuario

---
+++ app/repositories/user_repository.py
@@ -0,0 +1,40 @@
+from sqlalchemy.orm import Session
+from app.models.model import User
+from typing import Optional, List
+
+class UserRepository:
+    def __init__(self, db: Session):
+        self.db = db
+    
+    def create(self, user_data: dict) -> User:
+        user = User(**user_data)
+        self.db.add(user)
+        self.db.commit()
+        self.db.refresh(user)
+        return user
+    
+    def find_by_id(self, user_id: int) -> Optional[User]:
+        return self.db.query(User).filter(User.id == user_id).first()
+    
+    def find_by_email(self, email: str) -> Optional[User]:
+        return self.db.query(User).filter(User.email == email).first()
+    
+    def find_all(self) -> List[User]:
+        return self.db.query(User).all()
+    
+    def update(self, user_id: int, user_data: dict) -> Optional[User]:
+        user = self.find_by_id(user_id)
+        if user:
+            for key, value in user_data.items():
+                setattr(user, key, value)
+            self.db.commit()
+            self.db.refresh(user)
+        return user
+    
+    def delete(self, user_id: int) -> bool:
+        user = self.find_by_id(user_id)
+        if user:
+            self.db.delete(user)
+            self.db.commit()
+            return True
+        return False
