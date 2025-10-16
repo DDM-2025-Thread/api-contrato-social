7d8f6900186fb88c3594f1a1b104dcc02a8fb57c
ThallesSorrilha <thallessorrilha@gmail.com>
2025-10-16 16:48:29 -0300

    feature(gemini_service): adicionando classe de serviço referente à comunicação com o Gemini

---
+++ app/services/gemini_service.py
@@ -0,0 +1,41 @@
+from google import genai
+from google.genai import types
+from app.schemas.social_contract_squema import ContratoSocial
+import os
+import uuid
+from io import BytesIO
+
+
+class GeminiService:
+    def __init__(self, api_key: str):
+        self.client = genai.Client(api_key=api_key)
+        self.model_name = "gemini-2.5-flash"
+        self.prompt_context = (
+            "Given the text of a social contract, extract key information and return it in the provided JSON format. Discuss in Portuguese(BR)"
+        )
+
+    def upload_pdf_for_processing(self, pdf_file_bytes: bytes) -> types.File:
+        pdf_stream = BytesIO(pdf_file_bytes)
+        file_name = f"upload-{uuid.uuid4()}.pdf"
+        uploaded_file = self.client.files.upload(
+            file=pdf_stream,
+            config={
+                "display_name": file_name,
+                "mime_type": "application/pdf"
+            }
+        )
+        return uploaded_file
+
+    def get_contract_data(self, uploaded_file: types.File) -> dict:
+        try:
+            response = self.client.models.generate_content(
+                model=self.model_name,
+                contents=[uploaded_file, self.prompt_context],
+                config=types.GenerateContentConfig(
+                    response_mime_type="application/json",
+                    response_schema=ContratoSocial,
+                )
+            )
+            return response.text
+        finally:
+            self.client.files.delete(name=uploaded_file.name)
